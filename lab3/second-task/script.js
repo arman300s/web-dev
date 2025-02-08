663cdef67dea1998c86de247d9d0d02b31c2c410
arman300s <armanshoybek1@gmail.com>
2025-02-08 19:44:42 +0500

    to do list

---
+++ lab3/second-task/script.js
@@ -0,0 +1,26 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value == ""){
+        alert("Please enter a task");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(event){
+    if(event.target.tagName == "LI"){
+        event.target.classList.toggle("checked");
+    }
+    else if(event.target.tagName == "SPAN"){
+        event.target.parentElement.remove();
+    }
+},false);
